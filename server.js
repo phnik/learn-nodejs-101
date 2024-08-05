f34cf3d42bc31b55f3f4bd65e8fcb229b4fe69d0
Nguyen Manh Dat <22010220@st.phenikaa-uni.edu.vn>
2024-08-05 17:45:13 +0700

    learn about environment files, install nodemon

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const path = require('path')
+require('dotenv').config()
 
-// import express from 'express'
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME
 
 //config template engine
 // __dirname
 //app.set('views', path.join(__dirname, 'views'));
+
 app.set('views', './src/views/')
 app.set('view engine', 'ejs')
 
+//uat, .prod
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World with nodemon!')
 })
 
 app.get('/hoidanit', (req, res) => {
     res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
